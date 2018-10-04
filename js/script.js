072f3a3270859f075cc866a74c0c27c7d107b448
James Messinger <james.messinger@gmail.com>
2018-10-04 07:41:27 -0500

    Added the ability to toggle the tiles using the number keys

--- js/script.js
+++ js/script.js
@@ -1,44 +1,65 @@
+(function() {
 'use strict';
 
-// Wait until all the stylesheets and images have loaded before showing the icons.
-// This prevents a Flash of Unstyled Content (FOUC) and makes the animation less jerky,
-// since it's not loading images at the same time as animating
+
 window.onload = () => {
-  let iconList = document.getElementById('iconList');
-  iconList.classList.remove('hidden');
+  // Show the slot 1 tiles and links by default
+  showSlot(1);
 
-  // Some really cool animations are:
-  // flip, swing, jackInTheBox, rotateIn, tada, zoomIn, bounceInUp, flipInX,  flipInY, pulse
-  iconList.classList.add('animated', 'zoomIn');
+  // Wait until all the stylesheets and images have loaded before showing the tiles.
+  // This prevents a Flash of Unstyled Content (FOUC) and makes the animation less jerky,
+  // since it's not loading images at the same time as animating
+  showTiles();
 
-  // Add a click event handler to each of the icons
-  let icons = iconList.getElementsByTagName('a');
-  for (let icon of icons) {
-    icon.addEventListener('click', onTileClick);
-  }
+  // Whenever a number key is pressed, show the corresponding slot
+  window.addEventListener('keypress', showSlotForKey);
 };
 
 
 /**
- * If the "alt" key is pressed when a tile is clicked, then navigate to the "data-alt-href",
-  if any, instead of the "href"
+ * Animates the tiles onto the screen
  */
-function onTileClick(event) {
-  if (event.altKey) {
-    let icon = event.currentTarget;
-    let url = icon.getAttribute('data-alt-href') || icon.getAttribute('href');
+function showTiles() {
+  let tileList = document.getElementById('tileList');
+  tileList.classList.remove('hidden');
 
-    if (url) {
-      event.preventDefault();
+  // Some really cool animations are:
+  // flip, swing, jackInTheBox, rotateIn, tada, zoomIn, bounceInUp, flipInX,  flipInY, pulse
+  tileList.classList.add('animated', 'zoomIn');
+}
 
-      if (event.ctrlKey) {
-        // Open the link in a new tab
-        window.open(url);
-      }
-      else {
-        // Open the link in the current tab
-        window.location = url;
-      }
-    }
+
+/**
+ * Toggles HTML attributes to the corresponding slot.  For example, if slot === 1,
+ * then attributes like data-href-1 and data-src-1 will be activated.  If slot === 2,
+ * then attributes like data-href-2 and data-src-2 will be activated.
+ */
+function showSlot(slot) {
+  setAttribute('href', slot);
+  setAttribute('src', slot);
+}
+
+
+/**
+ * Sets the specified attribute to the corresponding "data-xxx-##" attribute for all elements
+ */
+function setAttribute(attribute, slot) {
+  let elements = document.querySelectorAll(`[data-${attribute}-${slot}]`);
+
+  for (let element of elements) {
+    let slotValue = element.getAttribute(`data-${attribute}-${slot}`);
+    element.setAttribute(attribute, slotValue);
   }
 }
+
+
+/**
+ * Whenever a number key is pressed, show the corresponding slot
+ */
+function showSlotForKey(event) {
+  if (/\d/.test(event.key)) {
+    // It's a number key, so show the corresponding slot
+    showSlot(event.key);
+  }
+}
+}());
